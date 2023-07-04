60ef9d6a6f994bb2aa995f1b2d44e6a9dd586c4b
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-07-04 22:10:54 +0800

    Update build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -10,7 +10,11 @@ echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
-# install packages
+# wget
+pacman -Syu --noconfirm --needed                       \
+    wget                                               \
+    
+# install R dependencies
 pacman -Syu --noconfirm --needed                       \
     base                                               \
     base-devel                                         \
